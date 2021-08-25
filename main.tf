cfdad448490e26c0f14b19c2e1a8d60c2cf8eb24
anil <anil@gmail.com>
2021-08-25 12:41:03 +0530

    formating code

--- main.tf
+++ main.tf
@@ -1,117 +1,117 @@
 
 resource "aws_vpc" "main" {
-    cidr_block = var.cidr
-    enable_dns_support = var.enable_dns_support
-    enable_dns_hostnames = var.enable_dns_hostnames
-    enable_classiclink = var.enable_classiclink
-    assign_generated_ipv6_cidr_block = var.assign_generated_ipv6_cidr_block
+  cidr_block                       = var.cidr
+  enable_dns_support               = var.enable_dns_support
+  enable_dns_hostnames             = var.enable_dns_hostnames
+  enable_classiclink               = var.enable_classiclink
+  assign_generated_ipv6_cidr_block = var.assign_generated_ipv6_cidr_block
 
-    tags = {
-      BusinessUnit = var.business_unit
-      Name = var.name
-      App = var.app_tag
-      ManagedBy = var.managed_by
-      Environment = var.environment
-      Role = "Main VPC"
-      Provisioner = "Terraform"
-    }
-  
+  tags = {
+    BusinessUnit = var.business_unit
+    Name         = var.name
+    App          = var.app_tag
+    ManagedBy    = var.managed_by
+    Environment  = var.environment
+    Role         = "Main VPC"
+    Provisioner  = "Terraform"
+  }
+
 }
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
   tags = {
-      BusinessUnit = var.business_unit
-      Name = var.name
-      App = var.app_tag
-      ManagedBy = var.managed_by
-      Environment = var.environment
-      Role = "Main Internet Gateway"
-      Provisioner = "Terraform"
-    }
+    BusinessUnit = var.business_unit
+    Name         = var.name
+    App          = var.app_tag
+    ManagedBy    = var.managed_by
+    Environment  = var.environment
+    Role         = "Main Internet Gateway"
+    Provisioner  = "Terraform"
+  }
 }
 
 resource "aws_eip" "nat" {
   count = length(var.dmz_standard_subnet_cidrs)
-  vpc = true
+  vpc   = true
 
   tags = {
-      BusinessUnit = var.business_unit
-      Name = var.name
-      App = var.app_tag
-      ManagedBy = var.managed_by
-      Environment = var.environment
-      Role = "NAT EIP"
-      Provisioner = "Terraform"
-    }
+    BusinessUnit = var.business_unit
+    Name         = var.name
+    App          = var.app_tag
+    ManagedBy    = var.managed_by
+    Environment  = var.environment
+    Role         = "NAT EIP"
+    Provisioner  = "Terraform"
+  }
 }
 
 resource "aws_nat_gateway" "main" {
-  count = length(var.dmz_standard_subnet_cidrs)
-  allocation_id = element(aws_eip.nat.*.id, count.index )
-  subnet_id = element(module.dmz_standard_subnet.subnet_ids, count.index)
-  depends_on = [aws_internet_gateway.main]
+  count         = length(var.dmz_standard_subnet_cidrs)
+  allocation_id = element(aws_eip.nat.*.id, count.index)
+  subnet_id     = element(module.dmz_standard_subnet.subnet_ids, count.index)
+  depends_on    = [aws_internet_gateway.main]
 
   tags = {
-      BusinessUnit = var.business_unit
-      Name = var.name
-      App = var.app_tag
-      ManagedBy = var.managed_by
-      Environment = var.environment
-      Role = "main NAT Gateway"
-      Provisioner = "Terraform"
-    }
+    BusinessUnit = var.business_unit
+    Name         = var.name
+    App          = var.app_tag
+    ManagedBy    = var.managed_by
+    Environment  = var.environment
+    Role         = "main NAT Gateway"
+    Provisioner  = "Terraform"
+  }
 }
 
 
 module "dmz_senitive_subnet" {
   source = "git::ssh://git@github.com:anil-shiv/tf_aws_public_subnet.git"
 
-  name = "${var.environment}-dmz_sensitive"
-  cidrs = var.dmz_sensitive_subnet_cidrs
-  azs = var.avalibility_zone
-  vpc_id = aws_vpc.main.id
-  igw_id = aws_internet_gateway.main.id
-  business_unit = var.business_unit
-  env = var.environment
-  app_tag = var.app_tag
-  managed_by = var.managed_by
+  name                = "${var.environment}-dmz_sensitive"
+  cidrs               = var.dmz_sensitive_subnet_cidrs
+  azs                 = var.avalibility_zone
+  vpc_id              = aws_vpc.main.id
+  igw_id              = aws_internet_gateway.main.id
+  business_unit       = var.business_unit
+  env                 = var.environment
+  app_tag             = var.app_tag
+  managed_by          = var.managed_by
   data_classification = "sensitive"
-  tier = "dmz"
-  
+  tier                = "dmz"
+
 }
 
 module "dmz_standard_subnet" {
   source = "git::ssh://git@github.com:anil-shiv/tf_aws_public_subnet.git"
 
-  name = "${var.environment}-dmz_standard"
-  cidrs = var.dmz_standard_subnet_cidrs
-  azs = var.avalibility_zone
-  vpc_id = aws_vpc.main.id
-  igw_id = aws_internet_gateway.main.id
-  business_unit = var.business_unit
-  env = var.environment
-  app_tag = var.app_tag
-  managed_by = var.managed_by
+  name                = "${var.environment}-dmz_standard"
+  cidrs               = var.dmz_standard_subnet_cidrs
+  azs                 = var.avalibility_zone
+  vpc_id              = aws_vpc.main.id
+  igw_id              = aws_internet_gateway.main.id
+  business_unit       = var.business_unit
+  env                 = var.environment
+  app_tag             = var.app_tag
+  managed_by          = var.managed_by
   data_classification = "standard"
-  tier = "dmz"
-  
+  tier                = "dmz"
+
 }
 
 module "db_standard_subnet" {
   source = "git::ssh://git@github.com:anil-shiv/tf_aws_private_subnet_nat_gateway.git"
-  
 
-  name = "${var.environment}-db_standard"
-  cidrs = var.db_standard_subnet_cidrs
-  azs = var.avalibility_zone
-  vpc_id = aws_vpc.main.id
-  nat_gateway_ids = aws_internet_gateway.main.*.id
-  public_subnet_ids = module.dmz_standard_subnet.subnet_ids
-  business_unit = var.business_unit
-  env = var.environment
-  app_tag = var.app_tag
-  managed_by = var.managed_by
+
+  name                = "${var.environment}-db_standard"
+  cidrs               = var.db_standard_subnet_cidrs
+  azs                 = var.avalibility_zone
+  vpc_id              = aws_vpc.main.id
+  nat_gateway_ids     = aws_internet_gateway.main.*.id
+  public_subnet_ids   = module.dmz_standard_subnet.subnet_ids
+  business_unit       = var.business_unit
+  env                 = var.environment
+  app_tag             = var.app_tag
+  managed_by          = var.managed_by
   data_classification = "standard"
-  tier = "db"
+  tier                = "db"
 }
--- variables.tf
+++ variables.tf
@@ -1,123 +1,123 @@
 variable "name" {
-    description = "Name tag eg stack"
+  description = "Name tag eg stack"
 }
 
 variable "region" {
-    description = "AWS reagion in Which resources are created, You must set the avalibility_zone variable as well if you define this value something other than the default"
+  description = "AWS reagion in Which resources are created, You must set the avalibility_zone variable as well if you define this value something other than the default"
 }
 
 variable "avalibility_zone" {
-    description = "A comma-separated list of avalibility zones"
-    type = list(string)
+  description = "A comma-separated list of avalibility zones"
+  type        = list(string)
 }
 
 variable "environment" {
-    description = "Environment tag, recommmendation: <account name>_<region>, e.g. prod_us-west-2"
+  description = "Environment tag, recommmendation: <account name>_<region>, e.g. prod_us-west-2"
 }
 
 variable "business_unit" {
-    description = "Business unit within organization (e.g. Consumer)"
+  description = "Business unit within organization (e.g. Consumer)"
 }
 
 variable "app_tag" {
-    description = "APP tag eg. Plateform, DevOps"
+  description = "APP tag eg. Plateform, DevOps"
 }
 
 variable "managed_by" {
-    description = "Managed by eg. Plateform, DevOps"
+  description = "Managed by eg. Plateform, DevOps"
 }
 
 variable "cidr" {
-    description = "CIDR block for VPC"
+  description = "CIDR block for VPC"
 }
 
 variable "enable_dns_support" {
-    default = "true"
+  default = "true"
 }
 
 variable "enable_classiclink" {
-    default = "false"
+  default = "false"
 }
 
 variable "enable_dns_hostname" {
-    default = "true"
+  default = "true"
 }
 
 variable "assign_generated_ipv6_cird_block" {
-    default = "false"
+  default = "false"
 }
 
 variable "aws_vpn_gateway" {
-    description = "Bool to add a vpn gateway to the vpc or not. A VPN gateway is needed to use Directconnect. Default : 0"
-    default = "0"
+  description = "Bool to add a vpn gateway to the vpc or not. A VPN gateway is needed to use Directconnect. Default : 0"
+  default     = "0"
 }
 
 variable "dmz_standard_subnet_cidrs" {
-    description = "List of subnet block for dmz subnet for 'standard' data"
-    type = list(string)
-    default = []
+  description = "List of subnet block for dmz subnet for 'standard' data"
+  type        = list(string)
+  default     = []
 }
 
 variable "dmz_sensitive_subnet_cidrs" {
-    description = "List of subnet block for dmz subnet for 'sensitive' data"
-    type = list(string)
-    default = []
+  description = "List of subnet block for dmz subnet for 'sensitive' data"
+  type        = list(string)
+  default     = []
 }
 
 variable "web_standard_subnet_cidrs" {
-    description = "List of subnet block for web subnet for 'standard' data"
-    type = list(string)
-    default = []
+  description = "List of subnet block for web subnet for 'standard' data"
+  type        = list(string)
+  default     = []
 }
 
 variable "web_sensitive_subnet_cidrs" {
-    description = "List of subnet block for web subnet for 'sensitive' data"
-    type = list(string)
-    default = []
+  description = "List of subnet block for web subnet for 'sensitive' data"
+  type        = list(string)
+  default     = []
 }
 
 variable "app_standard_subnet_cidrs" {
-    description = "List of subnet block for app subnet for 'standard' data"
-    type = list(string)
-    default = []
+  description = "List of subnet block for app subnet for 'standard' data"
+  type        = list(string)
+  default     = []
 }
 
 variable "app_sensitive_subnet_cidrs" {
-    description = "List of subnet block for app subnet for 'sensitive' data"
-    type = list(string)
-    default = []
+  description = "List of subnet block for app subnet for 'sensitive' data"
+  type        = list(string)
+  default     = []
 }
 
 variable "db_standard_subnet_cidrs" {
-    description = "List of subnet block for db subnet for 'standard' data"
-    type = list(string)
-    default = []
+  description = "List of subnet block for db subnet for 'standard' data"
+  type        = list(string)
+  default     = []
 }
 
 variable "db_sensitive_subnet_cidrs" {
-    description = "List of subnet block for db subnet for 'sensitive' data"
-    type = list(string)
-    default = []
+  description = "List of subnet block for db subnet for 'sensitive' data"
+  type        = list(string)
+  default     = []
 }
 
 variable "admin_subnet_cidrs" {
-    description = "list of subnet blocks for admin subnet for 'admin' data"
-    type = list(string)
-    default = []
+  description = "list of subnet blocks for admin subnet for 'admin' data"
+  type        = list(string)
+  default     = []
 }
 
 variable "tgw_subnet_cidrs" {
-    description = "list of subnet block for tgw subnet for 'tgw' data"
-    type = list(string)
-    default = []
+  description = "list of subnet block for tgw subnet for 'tgw' data"
+  type        = list(string)
+  default     = []
 }
 
 variable "enable_endpoints_s3" {
-    description = "create a S3 VPC endpoint for all Subnets"
-    default = "true"
+  description = "create a S3 VPC endpoint for all Subnets"
+  default     = "true"
 }
 
 variable "enable_endpoints_dynamodb" {
-    description = "create a Dynamodb VPC endpoint for all Subnets"
-    default = "true"
+  description = "create a Dynamodb VPC endpoint for all Subnets"
+  default     = "true"
 }
